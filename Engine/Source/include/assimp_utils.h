922c5d6b5ea65c4d8f59fbd03662be56de46731c
Gabriel Ivanica <ivanicagabriel@gmail.com>
2015-04-13 14:55:12 +0300

    solved bug - assimp to glm matrix conversion

--- Engine/Source/include/assimp_utils.h
+++ Engine/Source/include/assimp_utils.h
@@ -19,10 +19,10 @@ namespace assimp {
 
 	inline void CopyMatix(const aiMatrix4x4 &mat, glm::mat4 &dest)
 	{
-		dest[0][0] = mat.a1; dest[0][1] = mat.a2; dest[0][2] = mat.a3; dest[0][3] = mat.a4;
-		dest[1][0] = mat.b1; dest[1][1] = mat.b2; dest[1][2] = mat.b3; dest[1][3] = mat.b4;
-		dest[2][0] = mat.c1; dest[2][1] = mat.c2; dest[2][2] = mat.c3; dest[2][3] = mat.c4;
-		dest[3][0] = mat.d1; dest[3][1] = mat.d2; dest[3][2] = mat.d3; dest[3][3] = mat.d4;
+		dest[0][0] = mat.a1; dest[0][1] = mat.b1; dest[0][2] = mat.c1; dest[0][3] = mat.d1;
+		dest[1][0] = mat.a2; dest[1][1] = mat.b2; dest[1][2] = mat.c2; dest[1][3] = mat.d2;
+		dest[2][0] = mat.a3; dest[2][1] = mat.b3; dest[2][2] = mat.c3; dest[2][3] = mat.d3;
+		dest[3][0] = mat.a4; dest[3][1] = mat.b4; dest[3][2] = mat.c4; dest[3][3] = mat.d4;
 	}
 
 
