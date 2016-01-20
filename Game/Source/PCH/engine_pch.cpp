2064e5178bb7a96aa25f875698cbfea2ad5d239c
Gabriel Ivanica <gabriel.ivanica@gmail.com>
2016-01-20 02:49:41 +0200

    removed unused project

--- Game/Source/PCH/engine_pch.cpp
+++
@@ -1,7 +0,0 @@
-// pch.cpp : source file that includes just the standard includes
-// pch.obj will contain the pre-compiled type information
-
-#include "pch.h"
-
-// TODO: reference any additional headers you need in PCH.H
-// and not in this file
--- Game/Source/Physics/PhysX.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <PhysX/PxPhysicsAPI.h>
-#include <Physics/PhysXManager.h>
-#include <Physics/PhysXRagdoll.h>
-
-using namespace physx;
